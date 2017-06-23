91c2dec829d09e5fee5a385022097723a3b9aa82
praveen062 <praveenjinde2@gmail.com>
2017-06-23 11:49:49 +0530

    jackson configuration

--- src/main/java/com/praveen/praveen/datastructures/trees/api/TreesApiResource.java
+++ src/main/java/com/praveen/praveen/datastructures/trees/api/TreesApiResource.java
@@ -2,7 +2,9 @@ package com.praveen.praveen.datastructures.trees.api;
 
 
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
@@ -11,10 +13,10 @@ import javax.ws.rs.core.UriInfo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.praveen.praveen.datastructures.trees.domain.ReadStudentData;
-import com.praveen.praveen.datastructures.trees.domain.Student;
-import com.praveen.praveen.datastructures.trees.domain.StudentRepository;
 import com.praveen.praveen.support.service.ToApiJsonSerializer;
 
 @Path("hello")
@@ -34,18 +36,26 @@ public class TreesApiResource {
 
 	@GET
 	@Produces({ MediaType.APPLICATION_JSON })
-	public String getData(@Context final UriInfo uriInfo) {
+	public Employee getData(@Context final UriInfo uriInfo) {
 		try{
-		Employee  employee = new Employee(Long.valueOf(1), "praveen", 1);
+		Employee  employee = new Employee(null, "praveen", 1);
 		System.out.println("the employee data is ============="+employeeData);
-	
+		this.data.saveStudent();
 		
 		
-		return this.employeeData.serialize(this.data.saveStudent());
+		return employee;
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
-		return "{}";
+		return null;
 	}
 
+	
+	@POST
+	@Consumes({MediaType.APPLICATION_JSON})
+	@Produces({ MediaType.APPLICATION_JSON })
+	public Employee postData(@RequestBody Employee data){
+		System.out.println("the data is data" +data);
+		return data;
+	}
 }
--- src/main/java/com/praveen/praveen/support/WebXmlConfiguration.java
+++ src/main/java/com/praveen/praveen/support/WebXmlConfiguration.java
@@ -1,10 +1,20 @@
 package com.praveen.praveen.support;
+import java.util.List;
+
 import javax.servlet.Servlet;
 
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.jersey.api.core.PackagesResourceConfig;
 import com.sun.jersey.spi.spring.container.servlet.SpringServlet;
 
@@ -12,7 +22,7 @@ import com.sun.jersey.spi.spring.container.servlet.SpringServlet;
 
 
 @Configuration
-public class WebXmlConfiguration {
+public class WebXmlConfiguration extends WebMvcConfigurationSupport  {
 
 
     @Bean
@@ -30,6 +40,9 @@ public class WebXmlConfiguration {
                 ResponseCorsFilter.class.getName());
         jerseyServletRegistration.addInitParameter(PackagesResourceConfig.PROPERTY_PACKAGES, "com.praveen.praveen");
         jerseyServletRegistration.addInitParameter("com.sun.jersey.config.feature.DisableWADL", "true");
+		 
+		// @formatter:on
+
         System.out.println("end of spring config");
         // debugging for development:
         // jerseyServletRegistration.addInitParameter("com.sun.jersey.spi.container.ContainerRequestFilters",
@@ -37,5 +50,21 @@ public class WebXmlConfiguration {
         return jerseyServletRegistration;
     }
     
+    
+    @Bean
+    public MappingJackson2HttpMessageConverter customJackson2HttpMessageConverter() {
+     MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+     ObjectMapper objectMapper = new ObjectMapper();
+     objectMapper.setVisibility(PropertyAccessor.ALL, Visibility.ANY);
+     objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+     objectMapper.setSerializationInclusion(Include.NON_NULL);
+     jsonConverter.setObjectMapper(objectMapper);
+     return jsonConverter;
+    }
+    
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+     converters.add(customJackson2HttpMessageConverter());
+    }
     
 }
